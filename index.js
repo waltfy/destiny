f7601c9278f66c7db6b5b6f1314f596adc2c6333
Walter Carvalho <walter@gitter.im>
2014-11-23 20:39:08 +0000

    added option to override host

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-var Destiny = require('./lib/destiny');
-console.log('Destiny:', Destiny);
-
-Destiny
-  .search({ type: 1, name: 'tomphuu' })
-  .then(function (result) {
-    console.log('result:', result);
-  });
+module.exports = require('./lib/destiny');
