8794f07fdc4228c3f1a5fdc88fbf3eb0f7dbf73d
Walter Carvalho <walter@gitter.im>
2014-12-02 00:35:22 +0000

    FIX index export

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-// module.exports = require('./lib/destiny');
-var client = require('./lib/destiny')({ host: 'http://localhost:9000' });
-
-client
-  .main({ type: 1, membership: '4611686018439531850', id: "2305843009242314105" }).end()
-  .then(function (res) {
-    console.log(res.body);
-  });
+module.exports = require('./lib/destiny');
